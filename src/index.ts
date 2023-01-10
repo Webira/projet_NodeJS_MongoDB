31a0355bb18d1987cc6df7951f821b7cafb99146
Webira <guery.ira@gmail.com>
2023-01-10 13:32:03 +0100

    API fastify calculatrice

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-//console.log('Coucou')
-console.log(`Bonjour ${process.env.FIRSTNAME} ${process.env.LASTNAME}, Comment allez-vous ? Bien j'espère !`)
+import fastify from 'fastify'
+
+// Création d'une application fastify
+const app = fastify()
+
+// Écoute un port et un host
+app.listen({ port: 5353, host: '127.0.0.1' }, () => {
+    // Affiche un message dans la console nous indiquant que le serveur est démarré
+    console.log("Le serveur http est prêt sur l'address : http://127.0.0.1:5353")
+  })
